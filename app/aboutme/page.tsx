5919a5e3bafbea871a58af9d6976390d881da4ab
z5376268 <z5376268>
2023-09-07 03:27:11 +1000

    this should work

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -39,108 +39,12 @@ export default function Home() {
   const [animationConfig, setAnimationConfig] = useState(config.gentle);
   const names = ['Jordan J.', 'JJ.'];
   const roles = ['Cyber Security.', 'Backend.', 'Frontend.']
-
-  let details = window.navigator.userAgent
-  let regexp = /android|iphone|kindle|ipad/i;
-  let isMobileDevice = regexp.test(details);
-
-  useEffect(() => {
-    if (isMobileDevice) {
-      const imageDiv = document.querySelector("#imageDiv") as HTMLDivElement;
-      if (imageDiv) {
-        imageDiv.style.setProperty('display', 'none');
-      }
-    }
-  }, [isMobileDevice]);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
   return (
     <ParallaxProvider>
       <ThreeParticleComponent />
       <main className={styles.root}>
-      <DynamicNavbar />
-      <Parallax translate='yes' translateY={[-25,25]}>
-        {/**
-         * https://github.com/klevron/threejs-toys
-         */}
-         <div className={styles.outerDiv}>
-        <div className={styles.textDiv}>
-            <div className={styles.innerTextDiv}>
-            <Typography variant="h3" align="left" color='#e3e3e3' className={styles.textName}>
-              Hi, I&apos;m&nbsp;
-                <Typed strings={
-                  names
-                } 
-                typeSpeed={100}
-                backSpeed={70}
-                loop
-                backDelay={3000}
-                style={{
-                  textShadow: '0px 0px 8px #cb997e',
-                }}
-                />
-            </Typography>
-            <Typography variant="body1" component='div' align="left" color='#e3e3e3' className={styles.myText}>
-              <Box>I&apos;m currently a Computer Science student at the University of New South Wales.</Box>
-              <Box>I&apos;m interested in&nbsp;
-                <Typed strings={
-                  roles
-                } 
-                typeSpeed={90}
-                backSpeed={30}
-                loop
-                backDelay={1000}
-                style={{
-                  textShadow: '0px 0px 10px #cdb4db',
-                }}
-                /></Box>
-            </Typography>
-            </div>
-        </div>
-        <Box className={styles.imageDiv}>
-          <DynamicBlob />
-        </Box>
-        </div>
-        <div style={{ 
-          height: '20dvh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-          <FancyButton ButtonText='SHIFT COLORS'/>
-        </div>
-      </Parallax>
-      <div className={styles.projectDiv} style={{ padding: '20px' }}>
-        <Typography variant="h3" align="left" color='#e3e3e3' style={{
-          width: '100%',
-          marginBottom: '20px'
-        }}>
-            Experiences, Skills, and Achievements
-          </Typography>
-          <Grid container rowSpacing={1} columns={{ xs: 4, sm: 8, md: 12 }} className={styles.gridprojects}>
-            {slides.map((item) => (
-              <Grid xs={4} sm={4} md={4} key={item.key} style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-                <ListItem alignItems='center' style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                }}>
-              <ElasticCarousel objectfit={item.objectfit} key={item.key} imageUrl={item.image} title={item.content} details={item.details} />
-              </ListItem>
-            </Grid>
-            ))}
-          </Grid>
-      </div>
-      <Parallax className={styles.contact}>
-
-      </Parallax>
-    </main>
+        <DynamicNavbar />
+      </main>
     </ParallaxProvider>
   )
 }

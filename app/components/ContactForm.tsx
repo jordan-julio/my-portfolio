68893fdf456c1d5cb3e05647901a3ff86739b76b
z5376268 <z5376268>
2023-12-02 14:40:53 +1100

    fix styling of innterTextDiv

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from './ContactForm.module.css'
 import { TextareaAutosize } from '@mui/material';
